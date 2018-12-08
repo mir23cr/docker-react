aa4f9e96b6186cf67b4b5967bba1303ddbfe662b
mir23cr <vladimir.aguilar@gorillalogic.com>
2018-12-08 13:34:42 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -11,15 +11,10 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <p className="App-intro">
+          Hi everyone! Mamafitz
+        </p>
       </div>
     );
   }
